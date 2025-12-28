4025031bfe1e1c4a2b93e140b1e172ceea22b75f
hadise76 <hadisekeykha@gmail.com>
2025-12-28 14:50:45 +0330

    ishk

--- app/SVGs/categoryslider/RevolveIcon.tsx
+++ app/SVGs/categoryslider/RevolveIcon.tsx
@@ -13,43 +13,43 @@ export default function RevolveIcon({ className }: IconProps) {
       className={className}
     >
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         clipRule="evenodd"
         d="M26.2972 12.4918C28.9993 11.1197 30.1949 9.46031 30.1949 8C30.1949 6.53969 28.9993 4.88031 26.2972 3.50818C23.6666 2.17233 19.941 1.30769 15.7538 1.30769C11.5667 1.30769 7.84105 2.17233 5.21046 3.50818C2.50842 4.88031 1.47692 6.53969 1.47692 8C1.47692 9.46032 2.50842 11.1197 5.21046 12.4918C7.84105 13.8277 11.5667 14.6923 15.7538 14.6923C19.941 14.6923 23.6666 13.8277 26.2972 12.4918ZM15.7538 16C24.4545 16 32 12.4183 32 8C32 3.58172 24.4545 0 15.7538 0C7.05324 0 0 3.58172 0 8C0 12.4183 7.05324 16 15.7538 16Z"
         fill="currentColor"
       />
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         clipRule="evenodd"
         d="M26.2972 36.4918C28.9993 35.1197 30.1949 33.4603 30.1949 32C30.1949 30.5397 28.9993 28.8803 26.2972 27.5082C23.6666 26.1723 19.941 25.3077 15.7538 25.3077C11.5667 25.3077 7.84105 26.1723 5.21046 27.5082C2.50842 28.8803 1.47692 30.5397 1.47692 32C1.47692 33.4603 2.50842 35.1197 5.21046 36.4918C7.84105 37.8277 11.5667 38.6923 15.7538 38.6923C19.941 38.6923 23.6666 37.8277 26.2972 36.4918ZM15.7538 40C24.4545 40 32 36.4183 32 32C32 27.5817 24.4545 24 15.7538 24C7.05324 24 0 27.5817 0 32C0 36.4183 7.05324 40 15.7538 40Z"
         fill="currentColor"
       />
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         clipRule="evenodd"
         d="M-3.24621e-08 32L0 8L1 8L1 32L-3.24621e-08 32Z"
         fill="currentColor"
       />
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         clipRule="evenodd"
         d="M31 32V8H32V32H31Z"
         fill="currentColor"
       />
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         clipRule="evenodd"
         d="M24 38V14H25V38H24Z"
         fill="currentColor"
       />
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         clipRule="evenodd"
         d="M6 38V14H7V38H6Z"
         fill="currentColor"
       />
       <path
-        fill-rule="evenodd"
+        fillRule="evenodd"
         clipRule="evenodd"
         d="M16 25V16H17V25H16Z"
         fill="currentColor"
